69f01e81410fe54a579e486c9763a8b487c0e47a
Timo S <tstaudinger@live.de>
2019-11-06 11:49:47 +0100

    Final Version

--- waehrungsrechner.c
+++ waehrungsrechner.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 float usd,amnt,spd = 0.0;
-int whrng = 0;
-char *whrng1,*whrng2 = "";
+int whrng,zwhrng = 0;
+char *whrng1,*whrng2,*zwhrngs = "";
 
 int main(void){
     printf("Wie viele USD bekomme ich für einen Euro?"); //1.11 USD
@@ -11,6 +11,8 @@ int main(void){
     scanf("%f",&spd);
     printf("Was ist Ihre Ausgangswährung? 1=Euro 2=USD 3=SPD:");
     scanf("%d",&whrng);
+    printf("Was ist Ihre Zielwährung? 1=Euro 2=USD 3=SPD:");
+    scanf("%d",&zwhrng);
     switch(whrng){
         case 1: whrng1 = "Euro";break;
         case 2: whrng1 = "USD";break;
@@ -25,6 +27,12 @@ int main(void){
         case 3: amnt = amnt / spd;break;
         default: amnt = amnt;
     }
-    printf("Sie erhalten dafuer %.2f Euro\n",amnt);
+    switch(zwhrng){
+        case 1: amnt = amnt;zwhrngs = "Euro";break;
+        case 2: amnt = amnt * usd;zwhrngs = "USD";break;
+        case 3: amnt = amnt * spd;zwhrngs = "SPD";break;
+        default: amnt = amnt;
+    }   
+    printf("Sie erhalten dafuer %.2f %s\n",amnt,zwhrngs);
     return 0;
 }
