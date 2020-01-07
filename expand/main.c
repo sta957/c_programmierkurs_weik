6134730510ca333ee50c3b1bce02ea3965535475
Timo S <tstaudinger@live.de>
2020-01-07 13:29:20 +0100

    added print of standalone 92

--- expand/main.c
+++ expand/main.c
@@ -11,32 +11,42 @@
 int main(void){
     char* content;
     FILE *file;
-    int i, t, last;
-    file = fopen("/Users/statim/Desktop/weik/expand/test.txt","r");
+    int i, t, last, flag;
+    char* filename;
+
+    filename = "/Users/statim/Desktop/weik/expand/test.txt";
     
+    if (filename == NULL){
+        printf("Wahle eine Datei zum einlesen: ");
+        //scanf("%s",&filename);
+    }
+    
+    file = fopen(filename,"r");
     printf("\n\n");
 
-    if(file == NULL){
-        printf("Wahle eine Datei zum einlesen: ");
-        //scanf("%s",&file);
-    }else{
-        while((i = fgetc(file)) != EOF){
-            if(last == 92 && i == 116){
-                for(int space = 8 - (t % 8); space > 0; space--){
-                    //printf("%d",space); // for debugging purposes
-                    printf(" ");
-                }
-            }else if(i != 92){
-                if(last == 10) 
-                    t = 0;
+    while((i = fgetc(file)) != EOF){
+        if(last == 92 && i == 116){
+            for(int space = 8 - (t % 8); space > 0; space--){
+                //printf("%d",space); // for debugging purposes
+                printf(" ");
+            }
+            flag = 0;
+            last = i;
+        }else if(i != 92){
+            if(last == 10) 
+                t = 0;
                 putchar(i);
                 t = t + 1;
                 last = i;
-            }else{
-                last = 92;
+        }else if(i == 92){
+            last = i;
+            if(flag == 92){
+                putchar(i);
+                flag = 0;
             }
+            flag = i;
         }
-        fclose(file);
-        printf("\n\n");
     }
+    fclose(file);
+    printf("\n\n");
 }
