f2a0273303a6e8c042c1b55e4218a02ab202c01b
Timo S <tstaudinger@live.de>
2020-01-07 20:21:47 +0100

    Initial upload of final version

--- expand/main.c
+++ expand/main.c
@@ -1,52 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "expand.h"
 
-// programm to read filecontent to cli
-/*int expand(char* filename){
+int main(int argc, char *argv[]){
     
-    return 0;
-}*/
+    char* filename = argv[1];
 
-int main(void){
-    char* content;
-    FILE *file;
-    int i, t, last, flag;
-    char* filename;
-
-    filename = "/Users/statim/Desktop/weik/expand/test.txt";
+    //filename = "/Users/statim/Desktop/weik/expand/test.txt"; // for testing purposes
     
     if (filename == NULL){
         printf("Wahle eine Datei zum einlesen: ");
-        //scanf("%s",&filename);
+        scanf("%s",filename);
     }
-    
-    file = fopen(filename,"r");
     printf("\n\n");
-
-    while((i = fgetc(file)) != EOF){
-        if(last == 92 && i == 116){
-            for(int space = 8 - (t % 8); space > 0; space--){
-                //printf("%d",space); // for debugging purposes
-                printf(" ");
-            }
-            flag = 0;
-            last = i;
-        }else if(i != 92){
-            if(last == 10) 
-                t = 0;
-                putchar(i);
-                t = t + 1;
-                last = i;
-        }else if(i == 92){
-            last = i;
-            if(flag == 92){
-                putchar(i);
-                flag = 0;
-            }
-            flag = i;
-        }
-    }
-    fclose(file);
+    expand(filename);
     printf("\n\n");
 }
